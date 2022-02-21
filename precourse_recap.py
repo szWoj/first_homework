9d12734e4a41c37dc15ba5426d0793eaadce864d
Szymon <wojdyla.szymon@gmail.com>
2022-02-21 16:55:32 +0000

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,19 @@
+number_01 = 1
+number_02 = 10
+
+
+word_01 = "Good afternoon"
+name = " Blake"
+float_number = 1.92
+
+number_sum = number_01 + number_02
+
+def fake_sum(num1, num2):
+    return num1 * num2
+
+def create_sentance(word_1, word_2):
+    return word_1 + word_2
+
+print(fake_sum(number_01, number_02))
+print(create_sentance(word_01, name))
+
